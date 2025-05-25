72a4aadc8e8883196652300c6bd9a33a5e47ad00
sui <1202ayasnppk@gmail.com>
2025-05-25 06:58:18 +0000

    chore: CI設定を更新
    chore: CIの修正
    
    chore: run actions lintの設定変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,11 +3,8 @@ export function Header() {
     <header className='bg-background py-6 border-b border-primary shadow-xl sticky top-0 z-50'>
       <div className='container mx-auto px-4'>
         <div className='flex justify-start'>
-          <a
-            href='/'
-            className='group transition-all duration-300 hover:scale-105'
-          >
-            <span className='text-2xl font-bold bg-gradient-to-r from-primary to-primary-light bg-clip-text text-transparent group-hover:from-primary-light group-hover:to-primary transition-all duration-300'>
+          <a href='/' className='group'>
+            <span className='text-2xl font-bold bg-gradient-to-r from-primary to-primary-light bg-clip-text text-transparent group-hover:from-primary-light group-hover:to-primary'>
               金髪ヒロイン.com
             </span>
           </a>
